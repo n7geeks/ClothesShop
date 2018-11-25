94749342d2190fb0c21274d10f2490c220dd7a71
Ahmed NFAIHI <anfaihi10@gmail.com>
2018-11-25 22:22:45 +0100

    database

--- resources/db/clothes_shop.sql
+++ resources/db/clothes_shop.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 25, 2018 at 08:07 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+-- Client :  127.0.0.1
+-- Généré le :  Dim 25 Novembre 2018 à 22:20
+-- Version du serveur :  10.1.13-MariaDB
+-- Version de PHP :  7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,24 +17,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `clothes_shop`
+-- Base de données :  `clothes_shop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Structure de la table `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `categories` (
+  `id` bigint(20) NOT NULL,
+  `title` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `categories`
+-- Contenu de la table `categories`
 --
 
 INSERT INTO `categories` (`id`, `title`) VALUES
@@ -46,20 +42,44 @@ INSERT INTO `categories` (`id`, `title`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Structure de la table `customers`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `customers`
+--
+
+INSERT INTO `customers` (`id`, `code`, `firstName`, `lastName`, `phone`, `address`, `email`) VALUES
+(11, 'JF53974', 'Ahmed', 'NFAIHI', '0643573987', 'TanTan', 'anfaihi10@gmail.com'),
+(12, 'E453295', 'Mohamed', 'BOUKHLIF', '0649056631', 'Marrakech', 'mohamed.boukhlif@gmail.com'),
+(13, 'JF53998', 'Youness', 'IBENCHEROUTEN', '0655648099', 'TanTan', 'y.bench@gmail.com'),
+(14, 'N117953', 'Afraa', 'FAIK', '0645631105', 'Essaouira', 'afraa.faik@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL,
   `customer_id` bigint(20) NOT NULL,
   `total` double(8,2) NOT NULL,
-  `created_at` timestamp NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `orders`
+-- Contenu de la table `orders`
 --
 
 INSERT INTO `orders` (`id`, `customer_id`, `total`, `created_at`) VALUES
@@ -68,41 +88,34 @@ INSERT INTO `orders` (`id`, `customer_id`, `total`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_lines`
+-- Structure de la table `order_lines`
 --
 
-DROP TABLE IF EXISTS `order_lines`;
-CREATE TABLE IF NOT EXISTS `order_lines` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `order_lines` (
+  `id` bigint(20) NOT NULL,
   `product_id` bigint(20) NOT NULL,
   `order_id` bigint(20) NOT NULL,
   `qty` int(11) NOT NULL,
-  `price` double(8,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_order_lines_products` (`product_id`),
-  KEY `fk_order_lines_orders` (`order_id`)
+  `price` double(8,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Structure de la table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `code` bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `products` (
+  `code` bigint(20) NOT NULL,
   `category_id` bigint(20) NOT NULL,
   `label` varchar(128) NOT NULL,
   `buyingPrice` double(8,2) NOT NULL,
   `sellingPrice` double(8,2) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  PRIMARY KEY (`code`),
-  KEY `fk_products_categories` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `products`
+-- Contenu de la table `products`
 --
 
 INSERT INTO `products` (`code`, `category_id`, `label`, `buyingPrice`, `sellingPrice`, `image`) VALUES
@@ -118,22 +131,87 @@ INSERT INTO `products` (`code`, `category_id`, `label`, `buyingPrice`, `sellingP
 (10, 5, 'Produit1', 500.00, 1000.00, 'C:\\eclipse-workspace\\TPsMrNaji\\clothes_shop\\resources\\images\\products\\1543109143270_44917868_557893624664857_4929024805283299328_n.jpg');
 
 --
--- Constraints for dumped tables
+-- Index pour les tables exportées
 --
 
 --
--- Constraints for table `order_lines`
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `order_lines`
+--
+ALTER TABLE `order_lines`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_order_lines_products` (`product_id`),
+  ADD KEY `fk_order_lines_orders` (`order_id`);
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`code`),
+  ADD KEY `fk_products_categories` (`category_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT pour la table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `order_lines`
+--
+ALTER TABLE `order_lines`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `code` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `order_lines`
 --
 ALTER TABLE `order_lines`
   ADD CONSTRAINT `fk_order_lines_orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_order_lines_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`code`);
 
 --
--- Constraints for table `products`
+-- Contraintes pour la table `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `fk_products_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
